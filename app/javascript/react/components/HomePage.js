d6468a536fb9ed33be08b2138e2871c723e71a92
Tony Nguyen <tonynguyen3645@gmail.com>
2021-05-07 09:57:34 -0400

    changes to home page styling

--- app/javascript/react/components/HomePage.js
+++ app/javascript/react/components/HomePage.js
@@ -5,20 +5,23 @@ import CardTile from "./cards/CardTile"
 
 const HomePage = props => {
   return (
-    <div className="grid-container">
-      <h1 className="home-title">Articards</h1>
-      <p className="home-description">A Matching & Memory Game</p>
-      <div className="grid-x">
-        <div className="cell small-12 medium-6">
-          <CardTile key={1} word="hello" photo="hello" />
-          <CardTile key={2} word="hello" photo="hello" />
-          <CardTile key={3} word="hello" photo="hello" />
+    <div className="home-container">
+      <div className="home-row">
+        <div className="home-col">
+          <h1>Articards</h1>
+          <h2>A Memory & Matching Game</h2>
+          <p>This is a description of the website.</p>
+          <Link to='/play' className="play-button">PLAY NOW!</Link>
         </div>
-
-        <div className="cell small-12 medium-6">
-          <Link to="/play" className="button alert home">PLAY NOW!</Link>
-          <button className="button alert home">SIGN UP</button>
-          <button className="button alert home">SIGN IN</button>
+        <div className="home-col">
+          <div className="home-card">
+          </div>
+          <div className="home-card">
+          </div>
+          <div className="home-card">
+          </div>
+          <div className="home-card">
+          </div>
         </div>
       </div>
     </div>
